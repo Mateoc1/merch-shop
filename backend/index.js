4d9f5bfc8a2a44ca227a7b7d60f9ef69c7af685d
chaza <mateo.chazarreta.et21.21@gmail.com>
2025-09-25 14:50:04 -0300

    primer commit

---
+++ backend/index.js
@@ -0,0 +1,13 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+
+app.get('/', (req, res) =>{
+    res.send('Hola mndo')
+});
+
+
+const port =process.env.port || 3000
+
+app.listen(port, () => console.log('http://localhost:${port}'));
+
