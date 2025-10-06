646b7ca297259ef191575f9ca3e942efbb73b4ec
rutispepito <rutispepito@gmail.com>
2025-10-06 00:36:39 -0300

    Updated artist-catalog-static.js

--- public/js/artist-catalog-static.js
+++ public/js/artist-catalog-static.js
@@ -2,32 +2,32 @@ let cart = JSON.parse(localStorage.getItem('cart')) || [];
 
 const artistProducts = {
     'duki': [
-        { id: 'duki-1', name: 'Remera Duki Negra', price: 15990, image: 'https://images.pexels.com/photos/8532616/pexels-photo-8532616.jpeg', artist: 'Duki', size: 'M', color: 'Negro' },
-        { id: 'duki-2', name: 'Hoodie Duki', price: 32990, image: 'https://images.pexels.com/photos/6898856/pexels-photo-6898856.jpeg', artist: 'Duki', size: 'M', color: 'Negro' },
-        { id: 'duki-3', name: 'Gorra Duki', price: 8990, image: 'https://images.pexels.com/photos/1124468/pexels-photo-1124468.jpeg', artist: 'Duki', size: 'Única', color: 'Negro' },
-        { id: 'duki-4', name: 'Remera Duki Blanca', price: 15990, image: 'https://images.pexels.com/photos/8532619/pexels-photo-8532619.jpeg', artist: 'Duki', size: 'M', color: 'Blanco' },
-        { id: 'duki-5', name: 'Mochila Duki', price: 19990, image: 'https://images.pexels.com/photos/2905238/pexels-photo-2905238.jpeg', artist: 'Duki', size: 'Única', color: 'Negro' }
+        { id: 'duki-1', name: 'Remera Duki Negra', price: 15990, image: 'https://acdn-us.mitiendanube.com/stores/001/097/570/products/duki1-a82cb04dbd511ec96b16790858395459-640-0.jpg', artist: 'Duki', size: 'M', color: 'Negro' },
+        { id: 'duki-2', name: 'Buzo Trap Duki', price: 32990, image: 'https://acdn-us.mitiendanube.com/stores/944/335/products/duki-2-ojo1-b409ab086dca94295b16069525378128-640-0.jpg', artist: 'Duki', size: 'M', color: 'Negro' },
+        { id: 'duki-3', name: 'Gorra Duki', price: 8990, image: 'https://acdn-us.mitiendanube.com/stores/001/473/111/products/gorra-trucker-duki-n1-338553cf980f0856ad16911593476862-640-0.jpg', artist: 'Duki', size: 'Única', color: 'Negro' },
+        { id: 'duki-4', name: 'Remera Duki Blanca', price: 15990, image: 'https://ih1.redbubble.net/image.5240148851.5808/ssrco,classic_tee,womens_02,fafafa:ca443f4786,front,product_square,x600.1.jpg', artist: 'Duki', size: 'M', color: 'Blanco' },
+        { id: 'duki-5', name: 'Mochila Duki', price: 19990, image: 'https://http2.mlstatic.com/D_NQ_NP_669773-MLU74166639031_012024-O.webp', artist: 'Duki', size: 'Única', color: 'Negro' }
     ],
     'emilia': [
-        { id: 'emilia-1', name: 'Remera Emilia Rosa', price: 14990, image: 'https://images.pexels.com/photos/8532620/pexels-photo-8532620.jpeg', artist: 'Emilia Mernes', size: 'M', color: 'Rosa' },
-        { id: 'emilia-2', name: 'Buzo Emilia', price: 28990, image: 'https://images.pexels.com/photos/6764040/pexels-photo-6764040.jpeg', artist: 'Emilia Mernes', size: 'M', color: 'Beige' },
-        { id: 'emilia-3', name: 'Riñonera Emilia', price: 7990, image: 'https://images.pexels.com/photos/2783873/pexels-photo-2783873.jpeg', artist: 'Emilia Mernes', size: 'Única', color: 'Rosa' },
-        { id: 'emilia-4', name: 'Remera Emilia Blanca', price: 13990, image: 'https://images.pexels.com/photos/8532617/pexels-photo-8532617.jpeg', artist: 'Emilia Mernes', size: 'M', color: 'Blanco' },
-        { id: 'emilia-5', name: 'Totebag Emilia', price: 9990, image: 'https://images.pexels.com/photos/2783873/pexels-photo-2783873.jpeg', artist: 'Emilia Mernes', size: 'Única', color: 'Beige' }
+        { id: 'emilia-1', name: 'Crop top Emilia', price: 14990, image: 'https://http2.mlstatic.com/D_NQ_NP_762794-MLA80516127023_112024-O.webp', artist: 'Emilia Mernes', size: 'M', color: 'Rosa' },
+        { id: 'emilia-2', name: 'Buzo Emilia', price: 28990, image: 'https://acdn-us.mitiendanube.com/stores/003/378/771/products/7-46ce46673d692b4d3717174636224947-640-0.jpg', artist: 'Emilia Mernes', size: 'M', color: 'Beige' },
+        { id: 'emilia-3', name: 'Tote bag Emilia', price: 7990, image: 'https://dcdn-us.mitiendanube.com/stores/005/146/649/products/totebag-emilia-a8aa225eab4e0521b117260137380414-480-0.jpg', artist: 'Emilia Mernes', size: 'Única', color: 'Beige' },
+        { id: 'emilia-4', name: 'Remera Emilia Blanca', price: 13990, image: 'https://acdn-us.mitiendanube.com/stores/819/313/products/04db-3f54f6fc9a808ecff817102634546162-1024-1024.png', artist: 'Emilia Mernes', size: 'M', color: 'Blanco' },
+        { id: 'emilia-5', name: 'Totebag Emilia', price: 9990, image: 'https://http2.mlstatic.com/D_NQ_NP_771987-MLA81926035617_012025-O.webp', artist: 'Emilia Mernes', size: 'Única', color: 'Beige' }
     ],
     'luck-ra': [
-        { id: 'luck-ra-1', name: 'Remera Luck Ra', price: 14990, image: 'https://images.pexels.com/photos/8532622/pexels-photo-8532622.jpeg', artist: 'Luck Ra', size: 'M', color: 'Negro' },
-        { id: 'luck-ra-2', name: 'Buzo Luck Ra', price: 29990, image: 'https://images.pexels.com/photos/6764045/pexels-photo-6764045.jpeg', artist: 'Luck Ra', size: 'M', color: 'Negro' },
-        { id: 'luck-ra-3', name: 'Gorra Luck Ra', price: 8990, image: 'https://images.pexels.com/photos/1040173/pexels-photo-1040173.jpeg', artist: 'Luck Ra', size: 'Única', color: 'Negro' },
-        { id: 'luck-ra-4', name: 'Remera Luck Ra Blanca', price: 14990, image: 'https://images.pexels.com/photos/8532615/pexels-photo-8532615.jpeg', artist: 'Luck Ra', size: 'M', color: 'Blanco' },
-        { id: 'luck-ra-5', name: 'Visera Luck Ra', price: 6990, image: 'https://images.pexels.com/photos/1152077/pexels-photo-1152077.jpeg', artist: 'Luck Ra', size: 'Única', color: 'Negro' }
+        { id: 'luck-ra-1', name: 'Remera Cuarteto Luck Ra', price: 14990, image: 'https://acdn-us.mitiendanube.com/stores/819/313/products/02db-20f2a82e097346c2ad17587394059025-1024-1024.png', artist: 'Luck Ra', size: 'M', color: 'Negro' },
+        { id: 'luck-ra-2', name: 'Buzo Luck Ra', price: 43990, image: 'https://www.memoestampados.com/wp-content/uploads/2025/04/D_929706-MLU83867635722_042025-F-600x628.jpg', artist: 'Luck Ra', size: 'M', color: 'Negro' },
+        { id: 'luck-ra-3', name: 'Gorra Luck Ra', price: 11990, image: 'https://http2.mlstatic.com/D_NQ_NP_961150-MLA78353450339_082024-O.webp', artist: 'Luck Ra', size: 'Única', color: 'Negro' },
+        { id: 'luck-ra-4', name: 'Remera Luck Ra', price: 14990, image: 'https://http2.mlstatic.com/D_NQ_NP_889499-MLA83969642187_042025-O.webp', artist: 'Luck Ra', size: 'M', color: 'Negro' },
+        { id: 'luck-ra-5', name: 'Remera blanca Luck Ra', price: 10990, image: 'https://http2.mlstatic.com/D_NQ_NP_975934-MLA81902971174_012025-O.webp', artist: 'Luck Ra', size: 'M', color: 'Blanco' }
     ],
     'tini': [
-        { id: 'tini-1', name: 'Remera Tini Violeta', price: 15490, image: 'https://images.pexels.com/photos/8532621/pexels-photo-8532621.jpeg', artist: 'Tini Stoessel', size: 'M', color: 'Violeta' },
-        { id: 'tini-2', name: 'Hoodie Tini', price: 31990, image: 'https://images.pexels.com/photos/6764043/pexels-photo-6764043.jpeg', artist: 'Tini Stoessel', size: 'M', color: 'Negro' },
-        { id: 'tini-3', name: 'Gorra Tini', price: 8490, image: 'https://images.pexels.com/photos/1152077/pexels-photo-1152077.jpeg', artist: 'Tini Stoessel', size: 'Única', color: 'Beige' },
-        { id: 'tini-4', name: 'Remera Tini Negra', price: 15490, image: 'https://images.pexels.com/photos/8532618/pexels-photo-8532618.jpeg', artist: 'Tini Stoessel', size: 'M', color: 'Negro' },
-        { id: 'tini-5', name: 'Botella Tini', price: 11990, image: 'https://images.pexels.com/photos/4968391/pexels-photo-4968391.jpeg', artist: 'Tini Stoessel', size: 'Única', color: 'Rosa' }
+        { id: 'tini-1', name: 'Remera Tini', price: 21490, image: 'https://acdn-us.mitiendanube.com/stores/001/209/361/products/template-reme-frente-f34eb754ff0159bb8817089772144049-1024-1024.png', artist: 'Tini Stoessel', size: 'M', color: 'Negro' },
+        { id: 'tini-2', name: 'Hoodie Tini', price: 41990, image: 'https://http2.mlstatic.com/D_NQ_NP_673015-MLA84080597923_042025-O.webp', artist: 'Tini Stoessel', size: 'M', color: 'Negro' },
+        { id: 'tini-3', name: 'Remera tour Tini', price: 15990, image: 'https://acdn-us.mitiendanube.com/stores/001/097/570/products/tini-tini1-6862000637c445313e16625609423837-640-0.jpg', artist: 'Tini Stoessel', size: 'M', color: 'Negro' },
+        { id: 'tini-4', name: 'Remera Tini Negra', price: 15490, image: 'https://acdn-us.mitiendanube.com/stores/001/097/570/products/whatsapp-image-2022-03-08-at-12-11-38-am1-d2ef9e09d06cd2419516468498026754-640-0.jpeg', artist: 'Tini Stoessel', size: 'M', color: 'Negro' },
+        { id: 'tini-5', name: 'Vinilo Tini', price: 20990, image: 'https://akamai.sscdn.co/letras/360x360/albuns/b/3/8/7/1946911745420054.jpg', artist: 'Tini Stoessel', size: 'Única', color: 'Negro' }
     ]
 };
 
